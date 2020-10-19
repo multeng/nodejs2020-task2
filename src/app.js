97fb97195d215156b405d6b81fa9453ec9babb5b
Vladimir Kormakov <cenizo.kht@gmail.com>
2020-10-19 21:38:29 +0300

    feat: add loggers

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const YAML = require('yamljs');
 const userRouter = require('./resources/users/user.router');
 const boardRouter = require('./resources/boards/boards.router');
 const taskRouter = require('./resources/tasks/tasks.router');
+const logger = require('./common/logger');
+const morgan = require('morgan');
+require('express-async-errors');
+const { INTERNAL_SERVER_ERROR, getStatusText } = require('http-status-codes');
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
@@ -21,13 +25,29 @@ app.use('/', (req, res, next) => {
   next();
 });
 
+morgan.token('body', req => JSON.stringify(req.body));
+morgan.token('query', req => JSON.stringify(req.query));
+
+app.use(
+  morgan(':method :status :url Query: :query Body: :body', {
+    stream: logger.stream
+  })
+);
+
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 app.use('/boards', taskRouter);
 
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
+  if (err.status) {
+    logger.error(err.message);
+    res.status(err.status).send(err.message);
+  } else {
+    logger.error(err.stack);
+    res
+      .status(INTERNAL_SERVER_ERROR)
+      .send(getStatusText(INTERNAL_SERVER_ERROR));
+  }
   next();
 });
 
